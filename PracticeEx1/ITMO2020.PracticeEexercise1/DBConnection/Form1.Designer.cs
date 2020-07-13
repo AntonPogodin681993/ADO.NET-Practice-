179304593241e02d3ab079e36f273963712d516e
AntonPogodin681993 <65868601+AntonPogodin681993@users.noreply.github.com>
2020-07-13 23:11:24 +0300

    Add files via upload

---
+++ PracticeEx1/ITMO2020.PracticeEexercise1/DBConnection/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+namespace DBConnection
+
+{
+    partial class Form1
+    {
+        //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+        SqlConnection connection = new SqlConnection();
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.работаСБазойДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подключитьсяКБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отключитьсяОтБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.асинхронноеПодключениеКБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокПодключенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.работаСБазойДанныхToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Работа с базой данных";
+            // 
+            // работаСБазойДанныхToolStripMenuItem
+            // 
+            this.работаСБазойДанныхToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.подключитьсяКБДToolStripMenuItem,
+            this.отключитьсяОтБДToolStripMenuItem,
+            this.асинхронноеПодключениеКБДToolStripMenuItem,
+            this.списокПодключенийToolStripMenuItem});
+            this.работаСБазойДанныхToolStripMenuItem.Name = "работаСБазойДанныхToolStripMenuItem";
+            this.работаСБазойДанныхToolStripMenuItem.Size = new System.Drawing.Size(184, 24);
+            this.работаСБазойДанныхToolStripMenuItem.Text = "Работа с базой данных";
+            this.работаСБазойДанныхToolStripMenuItem.Click += new System.EventHandler(this.работаСБазойДанныхToolStripMenuItem_Click);
+            // 
+            // подключитьсяКБДToolStripMenuItem
+            // 
+            this.подключитьсяКБДToolStripMenuItem.Name = "подключитьсяКБДToolStripMenuItem";
+            this.подключитьсяКБДToolStripMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.подключитьсяКБДToolStripMenuItem.Text = "Подключиться к БД";
+            this.подключитьсяКБДToolStripMenuItem.Click += new System.EventHandler(this.подключитьсяКБДToolStripMenuItem_Click);
+            // 
+            // отключитьсяОтБДToolStripMenuItem
+            // 
+            this.отключитьсяОтБДToolStripMenuItem.Name = "отключитьсяОтБДToolStripMenuItem";
+            this.отключитьсяОтБДToolStripMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.отключитьсяОтБДToolStripMenuItem.Text = "Отключиться от БД";
+            this.отключитьсяОтБДToolStripMenuItem.Click += new System.EventHandler(this.отключитьсяОтБДToolStripMenuItem_Click);
+            // 
+            // асинхронноеПодключениеКБДToolStripMenuItem
+            // 
+            this.асинхронноеПодключениеКБДToolStripMenuItem.Name = "асинхронноеПодключениеКБДToolStripMenuItem";
+            this.асинхронноеПодключениеКБДToolStripMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.асинхронноеПодключениеКБДToolStripMenuItem.Text = "Асинхронное подключение к БД";
+            this.асинхронноеПодключениеКБДToolStripMenuItem.Click += new System.EventHandler(this.асинхронноеПодключениеКБДToolStripMenuItem_Click);
+            // 
+            // списокПодключенийToolStripMenuItem
+            // 
+            this.списокПодключенийToolStripMenuItem.Name = "списокПодключенийToolStripMenuItem";
+            this.списокПодключенийToolStripMenuItem.Size = new System.Drawing.Size(320, 26);
+            this.списокПодключенийToolStripMenuItem.Text = "Список Подключений";
+            this.списокПодключенийToolStripMenuItem.Click += new System.EventHandler(this.списокПодключенийToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem работаСБазойДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подключитьсяКБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отключитьсяОтБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem асинхронноеПодключениеКБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокПодключенийToolStripMenuItem;
+    }
+}
+
